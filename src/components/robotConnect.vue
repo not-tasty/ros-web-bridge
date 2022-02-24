fc27203ab37a38309bc8f2e8aae7b392f36b7664
howhung <zxsedas@gmail.com>
2022-02-24 22:40:31 +0800

    init version

---
+++ src/components/robotConnect.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="connect" v-if="disabled">
+            <label for="RobotType">RobotType:</label>
+            <input type="text">
+            <label for="IP">IP:</label>
+            <input type="text">
+            <label for="Port">Port:</label>
+            <input type="text">
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'robotConnect',
+    props:{
+        disabled: Boolean
+    },
+}
+</script>
+<style scoped>
+.connect{
+            width: 280px;
+            height: 30%;
+            display: flex;
+            flex-wrap: wrap;
+            padding: 10px;
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            margin: auto;
+            backdrop-filter: blur(2px);
+            border: 1px solid black;
+            box-shadow: 0px 0px 40px black;
+        }
+        .connect > label{
+            color: wheat;
+        }
+        .connect > input{
+            width: 100%;
+            margin: 6px 0px;
+            font-size: 16px;
+        }
+</style>
---
+++ src/components/robotMenu.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="menu">
+        <div class="item" id="Connect" @click="toggle">Connect</div>
+        <div class="item">Control</div>
+        <div class="item">View</div>
+        <div class="item">Navigation</div>
+        <div class="item">task</div>
+    </div>
+    <robotConnect :disabled="close" @click="toggle"/>
+</template>
+<script>
+import robotConnect from '@/components/robotConnect.vue'
+export default {
+    name: 'robotMenu',
+    components: {
+        robotConnect,
+    },
+    data() {
+        return {
+            close: true
+        }
+    },
+    methods:{
+        toggle(){
+            this.close = !this.close
+        },
+     
+    }
+}
+</script>
+<style scoped>
+.menu {
+    display: flex;
+    position: fixed;
+    bottom: 0;
+    background-color: rgb(19, 77, 110);
+    width: 100%;
+    font-size: 0px;
+    border-radius: 10px 10px 0px 0px;
+    box-shadow: 0px -1px 10px 0.5px rgba(0, 0, 0, 0.8);
+    color: wheat;
+}
+
+.item {
+    width: 100%;
+    padding: 12px 8px;
+    font-size: 18px;
+    text-align: center;
+    cursor: pointer;
+}
+
+.item:hover {
+    transition: .3s;
+    background-color: rgb(235, 43, 36);
+    color: rgb(5, 0, 0);
+    border-radius: 5px;
+}
+</style>
